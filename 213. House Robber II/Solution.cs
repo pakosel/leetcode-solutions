04f3c1a17889bf321a2e835e0942662eb7cdec05
pawel <pawel753@gmail.com>
2020-04-28 16:46:03 +0200

    Better approach for # 213. House Robber II

--- 213. House Robber II/Solution.cs
+++ 213. House Robber II/Solution.cs
@@ -9,34 +9,30 @@ namespace HouseRobber2
         public int Rob(int[] nums)
         {
             int len = nums.Length;
-            if(len == 0)
+            if (len == 0)
                 return 0;
-            if(len == 1)
+            if (len == 1)
                 return nums[0];
-            if(len == 2)
+            if (len == 2)
                 return Math.Max(nums[0], nums[1]);
 
-            int last = nums[len - 1];
-            nums[len-1] = 0;
-            int max = Rob_noCycle(nums);
-            nums[len-1] = last;
+            int max = Rob_noCircle(nums, len - 1);
             nums[0] = 0;
-            max = Math.Max(max, Rob_noCycle(nums));
+            max = Math.Max(max, Rob_noCircle(nums, len));
 
             return max;
         }
 
-        public int Rob_noCycle(int[] nums)
+        public int Rob_noCircle(int[] nums, int len)
         {
-            int len = nums.Length;
-
             int max_minus2 = nums[0];
             int max_minus1 = Math.Max(nums[1], nums[0]);
-            int max_current = 0;
+            int max_current = max_minus1;
 
-            for(int i=2; i<len; i++)
+            for (int i = 2; i < len; i++)
             {
-                max_current = Math.Max(max_minus1, max_minus2+nums[i]);
+                max_current = Math.Max(max_minus1, max_minus2 + nums[i]);
+                var tmp = max_minus2;
                 max_minus2 = max_minus1;
                 max_minus1 = max_current;
             }
