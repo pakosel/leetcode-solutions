60b051e467ae8bf85aa41e9301e142ff5db7a509
pawel <pkoselski@gmail.com>
2020-05-22 22:09:55 +0200

    Working Solution for problem # 140. Word Break II

--- 140. Word Break II/Solution.cs
+++ 140. Word Break II/Solution.cs
@@ -7,37 +7,52 @@ namespace WordBreakII
 {
     public class Solution
     {
+        private Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
         public IList<string> WordBreak(string s, IList<string> wordDict)
         {
-            var res = new List<string>();
-            foreach(var c in s)
-                if(!wordDict.Any(w => w.Contains(c)))
-                    return res;
+            foreach (var c in s)
+                if (!wordDict.Any(w => w.Contains(c)))
+                    return new List<string>();
 
             var maxWordLen = wordDict.Max(w => w.Length);
             var minWordLen = wordDict.Min(w => w.Length);
 
-            BreakNextWords("", s, wordDict, res, minWordLen, maxWordLen);
-            return res;
+            return BreakNextWords(s, wordDict, minWordLen, maxWordLen);
         }
 
-        private void BreakNextWords(string candidate, string s, IList<string> wordDict, IList<string> res, int minWordLen, int maxWordLen)
+        private IList<string> BreakNextWords(string s, IList<string> wordDict, int minWordLen, int maxWordLen)
         {
-            if(candidate.Length > 0 && s.Length == 0)
-            {
-                res.Add(candidate.Trim());
-                return;
-            }
+            if (string.IsNullOrEmpty(s))
+                return new List<string>();
+
+            if (dict.ContainsKey(s))
+                return dict[s];
+
+            var list = new List<string>();
 
             for (int i = minWordLen; i <= maxWordLen; i++)
             {
-                if(i > s.Length)
+                if (i > s.Length)
                     break;
                 var sub = s.Substring(0, i);
                 if (wordDict.Contains(sub))
-                    BreakNextWords($"{candidate} {sub}", s.Substring(i), wordDict, res, minWordLen, maxWordLen);
+                {
+                    var restString = s.Substring(i);
+                    if (restString.Length == 0)
+                        list.Add($"{sub}");
+                    else
+                    {
+                        var rest = BreakNextWords(restString, wordDict, minWordLen, maxWordLen);
+                        if (rest.Count == 0)
+                            continue;
+                        foreach (var el in rest)
+                            list.Add($"{sub} {el}");
+                    }
+                }
             }
+            dict.Add(s, list);
+
+            return list;
         }
     }
-
 }
