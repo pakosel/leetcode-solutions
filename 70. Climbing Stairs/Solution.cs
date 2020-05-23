b84e5745c950d988e953ce36bcc3e49c74226409
pawel <pawel753@gmail.com>
2020-05-23 17:40:31 +0200

    Better approach for # 70. Climbing Stairs

--- 70. Climbing Stairs/Solution.cs
+++ 70. Climbing Stairs/Solution.cs
@@ -12,13 +12,19 @@ namespace ClimbingStairs
             if (n < 3)
                 return n;
 
-            int[] res = new int[n];
-            res[n - 1] = 1;
-            res[n - 2] = 2;
-            for (int i = n - 3; i >= 0; i--)
-                res[i] = res[i + 1] + res[i + 2];
+            int n_2 = 2;
+            int n_1 = 1;
+            int res = 0;
 
-            return res[0];
+            int i = 3;
+            while (i++ <= n)
+            {
+                res = n_2 + n_1;
+                n_1 = n_2;
+                n_2 = res;
+            }
+
+            return res;
         }
     }
 }
