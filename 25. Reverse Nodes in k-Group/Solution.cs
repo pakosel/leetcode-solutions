34b8b5128c8666c036f4561743dfd466a20de31d
pawel <pawel753@gmail.com>
2020-05-25 21:27:07 +0200

    Solution for # 25. Reverse Nodes in k-Group

---
+++ 25. Reverse Nodes in k-Group/Solution.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace ReverseNodesInGroup
+{
+    public class Solution
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            if (head == null || k == 1)
+                return head;
+                
+            var headTail = ReverseSublist(head, k);
+            var res = headTail.Item1;
+
+            while (headTail != null && headTail.Item2 != null && headTail.Item2.next != null)
+            {
+                var newHeadTail = ReverseSublist(headTail.Item2.next, k);
+                headTail.Item2.next = newHeadTail.Item1;
+                headTail = newHeadTail;
+            }
+
+            return res;
+        }
+
+        //returning Tuple<newHead, newTail>
+        private Tuple<ListNode, ListNode> ReverseSublist(ListNode head, int count)
+        {
+            if (head == null)
+                return null;
+            int idx = count;
+            var testLen = head.next;
+            while (testLen != null && idx-- > 1)
+                testLen = testLen.next;
+            if (idx > 1)
+                return new Tuple<ListNode, ListNode>(head, null);
+
+            var newTail = head;
+            var curr = head.next;
+            var prev = head;
+            while (curr != null && count-- > 1)
+            {
+                var tmp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = tmp;
+            }
+            newTail.next = curr;
+
+            return new Tuple<ListNode, ListNode>(prev, newTail);
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ 25. Reverse Nodes in k-Group/SolutionTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ReverseNodesInGroup
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] { new ListNode(1), 1, new ListNode(1) },
+            //[1,2] 2 [2,1]
+            new object[] { new ListNode(1) {next = new ListNode(2) }, 2,
+                           new ListNode(2) {next = new ListNode(1) } },
+            //[1,2,3,4] 2 [2,1,4,3]
+            new object[] { new ListNode(1) {next = new ListNode(2) {next = new ListNode(3) {next = new ListNode(4) }}}, 2,
+                           new ListNode(2) {next = new ListNode(1) {next = new ListNode(4) {next = new ListNode(3) }}} },
+            //[1,2,3,4] 3 [3,2,1,4]
+            new object[] { new ListNode(1) {next = new ListNode(2) {next = new ListNode(3) {next = new ListNode(4) }}}, 3,
+                           new ListNode(3) {next = new ListNode(2) {next = new ListNode(1) {next = new ListNode(4) }}} },
+            //[1,2,3,4,5] 5 [5,4,3,2,1]
+            new object[] { new ListNode(1) {next = new ListNode(2) {next = new ListNode(3) {next = new ListNode(4) { next = new ListNode(5) }}}}, 5,
+                           new ListNode(5) {next = new ListNode(4) {next = new ListNode(3) {next = new ListNode(2) { next = new ListNode(1) }}}} },
+            //[1,2,3,4,5,6] 3 [3,2,1,6,5,4]
+            new object[] { new ListNode(1) {next = new ListNode(2) {next = new ListNode(3) {next = new ListNode(4) { next = new ListNode(5) { next = new ListNode(6) }}}}}, 3,
+                           new ListNode(3) {next = new ListNode(2) {next = new ListNode(1) {next = new ListNode(6) { next = new ListNode(5) { next = new ListNode(4) }}}}} },
+            //[1,2,3,4,5,6,7] 3 [3,2,1,6,5,4,7]
+            new object[] { new ListNode(1) {next = new ListNode(2) {next = new ListNode(3) {next = new ListNode(4) { next = new ListNode(5) { next = new ListNode(6) { next = new ListNode(7)}}}}}}, 3,
+                           new ListNode(3) {next = new ListNode(2) {next = new ListNode(1) {next = new ListNode(6) { next = new ListNode(5) { next = new ListNode(4) { next = new ListNode(7)}}}}}} },
+            //[1,2,3,4,5,6,7,8] 2 [2,1,4,3,6,5,8,7]
+            new object[] { new ListNode(1) {next = new ListNode(2) {next = new ListNode(3) {next = new ListNode(4) { next = new ListNode(5) { next = new ListNode(6) { next = new ListNode(7) { next = new ListNode(8)}}}}}}}, 2,
+                           new ListNode(2) {next = new ListNode(1) {next = new ListNode(4) {next = new ListNode(3) { next = new ListNode(6) { next = new ListNode(5) { next = new ListNode(8) { next = new ListNode(7)}}}}}}} },
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(ListNode head, int k, ListNode expected)
+        {
+            var sol = new Solution();
+            var res = sol.ReverseKGroup(head, k);
+
+            while(res != null)
+            {
+                Assert.AreEqual(res.val, expected.val);
+                res = res.next;
+                expected = expected.next;
+            }
+            
+            Assert.IsNull(expected);
+        }
+    }
+}
