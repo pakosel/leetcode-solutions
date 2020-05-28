eee68d14416d49716a29130f33fb33bdbb7c0718
pawel <pawel753@gmail.com>
2020-05-28 15:37:02 +0200

    Solution for # 22. Generate Parentheses

---
+++ 22. Generate Parentheses/SolutionTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GenerateParentheses
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] { 0, new string[] {""} },
+            new object[] { 1, new string[] {"()"} },
+            new object[] { 2, new string[] {"(())","()()"} },
+            new object[] { 3, new string[] {"((()))","(()())","(())()","()(())","()()()"} },
+            new object[] { 4, new string[] {"(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"} },
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(int n, string[] expected)
+        {
+            var sol = new Solution();
+            var res = sol.GenerateParenthesis(n);
+
+            CollectionAssert.AreEquivalent(res, expected);
+        }
+    }
+}
