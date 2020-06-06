356584fae4ab03da999166fe23703a0125239f87
pawel <pawel753@gmail.com>
2020-06-06 11:58:20 +0200

    Additional test case added to 354. Russian Doll Envelopes

--- 354. Russian Doll Envelopes/SolutionTest.cs
+++ 354. Russian Doll Envelopes/SolutionTest.cs
@@ -15,6 +15,7 @@ namespace RussianDollEnvelopes
             new object[] {new int[][] { new int[] {1,1} }, 1 },
             new object[] {new int[][] {new int[] {5,4}, new int[] {6,4}, new int[] {6,7}, new int[] {2,3}}, 3 },
             new object[] {new int[][] {new int[] {2,100}, new int[] {3,200}, new int[] {4,300}, new int[] {5,500}, new int[] {5,400}, new int[] {5,250}, new int[] {6,370}, new int[] {6,360}, new int[] {7,380}}, 5 },
+            new object[] {new int[][] {new int[] {46,89}, new int[] {50,53}, new int[] {52,68}, new int[] {72,45}, new int[] {77,81}}, 3 },
         };
 
         [Test]
