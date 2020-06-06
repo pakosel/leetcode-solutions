7367f5386e20cf746d1459f7f05aa14118966d97
pawel <pawel753@gmail.com>
2020-06-06 13:58:01 +0200

    DP approach for # 322. Coin Change

--- 322. Coin Change/Solution.cs
+++ 322. Coin Change/Solution.cs
@@ -7,6 +7,34 @@ namespace CoinChange
 {
     public class Solution
     {
+        public int CoinChange(int[] coins, int amount)
+        {
+            if (amount < 1)
+                return amount;
+            Array.Sort(coins);
+
+            int len = coins.Length;
+            const int inf = int.MaxValue-1;
+            int[,] arr = new int[len+1, amount+1];
+            
+            for(int j=0; j<=amount; j++)
+                arr[0,j] = inf;
+
+            for(int i=1; i<=len; i++)
+                for(int j=1; j<=amount; j++)
+                    if(j < coins[i-1])
+                        arr[i,j] = arr[i-1,j];
+                    else if(j == coins[i-1])
+                        arr[i,j] = 1;
+                    else
+                        arr[i,j] = Math.Min(arr[i-1, j], j > coins[i-1] ? 1 + arr[i, j - coins[i-1]] : inf);
+
+            return arr[len, amount] == inf ? -1 : arr[len,amount];
+        }
+    }
+
+    public class Solution_Recursive
+    {
         Dictionary<int, int> dictChange = new Dictionary<int, int>();
 
         public int CoinChange(int[] coins, int amount)
--- 322. Coin Change/SolutionTest.cs
+++ 322. Coin Change/SolutionTest.cs
@@ -17,6 +17,7 @@ namespace CoinChange
             new object[] { new int[] {1,2,5}, 24, 6 },
             new object[] { new int[] {1,2,3,5}, 27, 6 },
             new object[] { new int[] {1,2,3,5}, 25, 5 },
+            new object[] { new int[] {186,419,83,408}, 6249, 20 },
         };
 
         [Test]
@@ -24,6 +25,16 @@ namespace CoinChange
         public void Test_Generic(int[] coins, int amount, int expected)
         {
             var sol = new Solution();
+            var res = sol.CoinChange(coins, amount);
+
+            Assert.AreEqual(res, expected);
+        }
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Recursive(int[] coins, int amount, int expected)
+        {
+            var sol = new Solution_Recursive();
             var res = sol.CoinChange(coins, amount);
 
             Assert.AreEqual(res, expected);
