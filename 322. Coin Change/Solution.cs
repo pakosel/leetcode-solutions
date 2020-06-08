919cae3831056b35e0079a3c112a326c17e00d0b
Pawel <pawel753@gmail.com>
2020-06-08 16:34:28 +0200

    Gitpod integration (#1)
    * Optimized DP solution for # 322. Coin Change
    
    * Gitpod integration cleanup

--- 322. Coin Change/Solution.cs
+++ 322. Coin Change/Solution.cs
@@ -21,6 +21,12 @@ namespace CoinChange
                 arr[0,j] = inf;
 
             for(int i=1; i<=len; i++)
+            {
+                if(coins[i-1] > amount)
+                {
+                    arr[len, amount] = arr[i-1, amount];
+                    break;
+                }
                 for(int j=1; j<=amount; j++)
                     if(j < coins[i-1])
                         arr[i,j] = arr[i-1,j];
@@ -28,6 +34,7 @@ namespace CoinChange
                         arr[i,j] = 1;
                     else
                         arr[i,j] = Math.Min(arr[i-1, j], j > coins[i-1] ? 1 + arr[i, j - coins[i-1]] : inf);
+            }
 
             return arr[len, amount] == inf ? -1 : arr[len,amount];
         }
