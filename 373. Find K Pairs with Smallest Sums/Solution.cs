c75838433d4631bc5c8881eee08d7fb834b5cd51
pawel <pawel753@gmail.com>
2020-06-25 12:54:20 +0200

    Better approach for 373. Find K Pairs with Smallest Sums

--- 373. Find K Pairs with Smallest Sums/Solution.cs
+++ 373. Find K Pairs with Smallest Sums/Solution.cs
@@ -13,17 +13,17 @@ namespace FindPairsWithSmallestSums
             int len2 = nums2.Length;
             int idx = 0;
 
-            Tuple<int, int>[] arr = new Tuple<int, int>[len1 * len2];
+            int[][] arr = new int[len1 * len2][];
             for (int i = 0; i < len1; i++)
                 for (int j = 0; j < len2; j++)
-                    arr[idx++] = new Tuple<int, int>(nums1[i], nums2[j]);
-            Array.Sort(arr, (e1, e2) => (e1.Item1 + e1.Item2).CompareTo(e2.Item1 + e2.Item2));
+                    arr[idx++] = new int[] { nums1[i], nums2[j] };
+            Array.Sort(arr, (e1, e2) => (e1[0] + e1[1]).CompareTo(e2[0] + e2[1]));
 
             idx = 0;
             IList<IList<int>> res = new List<IList<int>>();
             while (k > 0 && idx < arr.Length)
             {
-                res.Add(new int[] { arr[idx].Item1, arr[idx].Item2 });
+                res.Add(arr[idx]);
                 k--;
                 idx++;
             }
