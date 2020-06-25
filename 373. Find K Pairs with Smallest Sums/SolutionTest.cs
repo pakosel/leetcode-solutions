27494305b709296792a3a6c17a3565f6bf15a1fa
pawel <pawel753@gmail.com>
2020-06-25 15:37:24 +0200

    Additional test case added

--- 373. Find K Pairs with Smallest Sums/SolutionTest.cs
+++ 373. Find K Pairs with Smallest Sums/SolutionTest.cs
@@ -16,6 +16,7 @@ namespace FindPairsWithSmallestSums
             new object[] { new int[] {1,1,2}, new int[] {1,2,3}, 2, new int[][] {new int[] {1,1},new int[] {1,1}} },
             new object[] { new int[] {1,2}, new int[] {3}, 3, new int[][] {new int[] {1,3}, new int[] {2,3}} },
             new object[] { new int[] {1,1,2,3,4,5,6}, new int[] {1,1,1,1,1,1}, 18, new int[][] {new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {1,1}, new int[] {2,1}, new int[] {2,1}, new int[] {2,1}, new int[] {2,1}, new int[] {2,1}, new int[] {2,1}} },
+            new object[] { new int[] {1,1,2}, new int[] {1,2,3}, 10, new int[][] {new int[] {1,1}, new int[] {1,1}, new int[] {2,1}, new int[] {1,2}, new int[] {1,2}, new int[] {2,2}, new int[] {1,3}, new int[] {1,3}, new int[] {2,3}} },
         };
 
         [Test]
