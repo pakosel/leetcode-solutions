2dae38088868a7818557fecd749d4a7efd06eb4a
pawel <pawel753@gmail.com>
2020-06-29 15:34:35 +0200

    Better approach for # 15. 3Sum

--- 15. 3Sum/Solution.cs
+++ 15. 3Sum/Solution.cs
@@ -6,13 +6,14 @@ namespace ThreeSum
 {
     public class Solution
     {
+        /* Approach 1 */
         public IList<IList<int>> ThreeSum(int[] nums)
         {
             var outList = new List<IList<int>>();
 
             if(nums.Length < 3)
                 return outList;
-            
+
             Array.Sort(nums);
 
             Dictionary<int, int> values = new Dictionary<int, int>();
@@ -38,6 +39,47 @@ namespace ThreeSum
                         if(idx > i && idx > j)
                             outList.Add(new int[] { nums[i], nums[j], nums[idx] });
                     }
+                }
+            }
+
+            return outList;
+        }
+
+        /* Approach 2 */
+        public IList<IList<int>> ThreeSum_better(int[] nums)
+        {
+            var outList = new List<IList<int>>();
+
+            if (nums.Length < 3)
+                return outList;
+
+            Array.Sort(nums);
+
+            var len = nums.Length;
+            var sum = 0;
+            for (int i = 0; i < len - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                    continue;
+                int lookingFor = sum - nums[i];
+                int start = i + 1;
+                int end = len - 1;
+
+                while (start < end)
+                {
+                    int currentSum = nums[start] + nums[end];
+                    if (currentSum == lookingFor)
+                    {
+                        outList.Add(new int[] { nums[i], nums[start], nums[end] });
+                        end--;
+                        while (end > start && nums[end] == nums[end + 1]) end--; //skip duplicates
+                        start++;
+                        while (start < end && nums[start] == nums[start - 1]) start++; //skip duplicates
+                    }
+                    else if (currentSum > lookingFor)
+                        end--;
+                    else //if(currentSum < lookingFor)
+                        start++;
                 }
             }
 
