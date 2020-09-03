a589fea0dc97d5b0a69cd2f40f72f522fbe6a7ff
pawel <pawel753@gmail.com>
2020-09-03 15:14:43 +0200

    String inputs in unit test for # 1162. As Far from Land as Possible

--- 1162. As Far from Land as Possible/SolutionTest.cs
+++ 1162. As Far from Land as Possible/SolutionTest.cs
@@ -9,6 +9,36 @@ namespace AsFarFromLandAsPossible
     [TestFixture]
     public class SolutionTest
     {
+        private static readonly object[] testCasesStr =
+        {
+            new object[] {"[[0]]", -1},
+            new object[] {"[[1,0,1],[0,0,0],[1,0,1]]", 2},
+            new object[] {"[[1,0,0],[0,0,0],[0,0,0]]", 4},
+            new object[] {"[[0,0,0],[0,0,0],[0,0,0]]", -1},
+            new object[] {"[[0,1,0,0],[1,0,0,1],[0,0,0,1],[0,0,1,0]]", 2},
+        };
+
+        [Test]
+        [TestCaseSource("testCasesStr")]
+        public void Test_GenericStr(string gridStr, int expected)
+        {
+            int[][] grid;
+            var arr = gridStr.TrimStart('[').TrimEnd(']').Split("],[");
+            grid = new int[arr.Length][];
+            for(int i=0; i<arr.Length; i++)
+            {
+                var innerArr = arr[i].TrimStart('[').TrimEnd(']').Split(',');
+                grid[i] = new int[innerArr.Length];
+                for(int j=0; j<innerArr.Length; j++)
+                    grid[i][j] = int.Parse(innerArr[j]);
+            }
+
+            var sol = new Solution();
+            var res = sol.MaxDistance(grid);
+
+            Assert.AreEqual(res, expected);
+        }
+        
         private static readonly object[] testCases =
         {
             //[[0]]
@@ -25,7 +55,7 @@ namespace AsFarFromLandAsPossible
 
         [Test]
         [TestCaseSource("testCases")]
-        public void Test_Recursive(int[][] grid, int expected)
+        public void Test_Generic(int[][] grid, int expected)
         {
             var sol = new Solution();
             var res = sol.MaxDistance(grid);
