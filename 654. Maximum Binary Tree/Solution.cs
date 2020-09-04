87a6bc0c466c49e7665a1d5fb133fa68a43b9145
pawel <pawel753@gmail.com>
2020-09-04 20:19:09 +0200

    Solution for # 654. Maximum Binary Tree

---
+++ 654. Maximum Binary Tree/Solution.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace MaximumBinaryTree
+{
+    public class Solution
+    {
+        public TreeNode ConstructMaximumBinaryTree(int[] nums)
+        {
+            return BuildMaxNode(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode BuildMaxNode(int[] nums, int left, int right)
+        {
+            if (left == right)
+                return new TreeNode(nums[left]);
+            else if (left > right || right < left)
+                return null;
+
+            int maxIdx = FindMaxIdx(nums, left, right);
+            return new TreeNode(nums[maxIdx], BuildMaxNode(nums, left, maxIdx - 1), BuildMaxNode(nums, maxIdx + 1, right));
+        }
+
+        private int FindMaxIdx(int[] nums, int left, int right)
+        {
+            int maxIdx = left;
+
+            for (int i = left; i <= right; i++)
+                if (nums[i] > nums[maxIdx])
+                    maxIdx = i;
+            return maxIdx;
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ 654. Maximum Binary Tree/SolutionTest.cs
@@ -0,0 +1,51 @@
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MaximumBinaryTree
+{
+    [TestFixture]
+    public class MaximumBinaryTree
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] { "[3,2,1,6,0,5]", new TreeNode(6, new TreeNode(3, null, new TreeNode(2, null, new TreeNode(1))), new TreeNode(5, new TreeNode(0))) },
+            new object[] { "[1]", new TreeNode(1) },
+            new object[] { "[1,2]", new TreeNode(2, new TreeNode(1)) },
+            new object[] { "[1,2,3]", new TreeNode(3, new TreeNode(2, new TreeNode(1))) },
+            new object[] { "[1,3,2]", new TreeNode(3, new TreeNode(1), new TreeNode(2)) },
+            new object[] { "[13,2,1,6,0,5]", new TreeNode(13, null, new TreeNode(6, new TreeNode(2, null, new TreeNode(1)), new TreeNode(5, new TreeNode(0)))) },
+            new object[] { "[3,4,2,1,6,0,5]", new TreeNode(6, new TreeNode(4, new TreeNode(3), new TreeNode(2, null, new TreeNode(1))), new TreeNode(5, new TreeNode(0))) },
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string input, TreeNode expected)
+        {
+            var numsStr = input.TrimStart('[').TrimEnd(']').Split(',');
+            int[] nums = new int[numsStr.Length];
+            for(int i=0; i<numsStr.Length; i++)
+                nums[i] = int.Parse(numsStr[i]);
+
+            var sol = new Solution();
+            var res = sol.ConstructMaximumBinaryTree(nums);
+
+            Assert.IsTrue(CompareTreeNode(res, expected));
+        }
+
+        private bool CompareTreeNode(TreeNode node1, TreeNode node2)
+        {
+            if(node1 == null && node2 == null)
+                return true;
+            if(node1 != null && node2 != null)
+            {
+                return node1.val == node2.val 
+                        && CompareTreeNode(node1.left, node2.left) 
+                        && CompareTreeNode(node1.right, node2.right);
+            }
+            
+            return false;
+        }
+    }
+}
