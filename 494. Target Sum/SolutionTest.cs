3bffcc7b5f281468cf3a729f43160f020c3c2be3
pawel <pawel753@gmail.com>
2020-09-06 17:22:18 +0200

    Solution for # 494. Target Sum

---
+++ 494. Target Sum/SolutionTest.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TargetSum
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] { "[1]", 1, 1 },
+            new object[] { "[1]", 2, 0 },
+            new object[] { "[1,0]", 1, 2 },
+            new object[] { "[1,1,1,1,1]", 3, 5 },
+            new object[] { "[1,2,2]", 3, 1 },
+            new object[] { "[1,2,2,5]", 3, 0 },
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string input, int S, int expected)
+        {
+            var numsStr = input.TrimStart('[').TrimEnd(']').Split(',');
+            int[] nums = new int[numsStr.Length];
+            for(int i=0; i<numsStr.Length; i++)
+                nums[i] = int.Parse(numsStr[i]);
+
+            var sol = new Solution();
+            var res = sol.FindTargetSumWays(nums, S);
+
+            Assert.AreEqual(res, expected);
+        }
+    }
+}
