de7100fe585785e265a8cd5a7f4370dd088dbf59
pawel <pawel753@gmail.com>
2020-09-09 14:25:02 +0200

    Additional test case added

--- 144. Binary Tree Preorder Traversal/SolutionTest.cs
+++ 144. Binary Tree Preorder Traversal/SolutionTest.cs
@@ -16,6 +16,7 @@ namespace BinaryTreePreorderTraversal
             new object[] {new TreeNode(1, new TreeNode(4, new TreeNode(2)), new TreeNode(3)), new int[] {1,4,2,3}},
             //[7,5,9,4,6,8,10]
             new object[] {new TreeNode(7, new TreeNode(5, new TreeNode(4), new TreeNode(6)), new TreeNode(9, new TreeNode(8), new TreeNode(10))), new int[] {7,5,4,6,9,8,10}},
+            new object[] {new TreeNode(15, new TreeNode(12, new TreeNode(10, new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(7, new TreeNode(6), new TreeNode(8))), new TreeNode(11)), new TreeNode(13, null, new TreeNode(14))), new TreeNode(20, new TreeNode(18))), new int[] { 15,12,10,5,3,2,4,7,6,8,11,13,14,20,18} },
         };
 
         [Test]
