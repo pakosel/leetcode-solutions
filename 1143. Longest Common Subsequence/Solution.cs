dfe79ecb9678aa1c5e3f03f44b1af0ae6880a7ca
pawel <pawel753@gmail.com>
2020-09-12 21:28:21 +0200

    Performance improvements for #1143

--- 1143. Longest Common Subsequence/Solution.cs
+++ 1143. Longest Common Subsequence/Solution.cs
@@ -13,19 +13,21 @@ namespace LongestCommonSubsequence
             int len2 = text2.Length;
             int[,] arr = new int[len1, len2];
 
-            for (int i = 0; i < len1; i++)
-                for (int j = 0; j < len2; j++)
+            if(text1[0] == text2[0])
+                arr[0,0] = 1;
+
+            //initialize first row and first column
+            for(int i=1; i<len1; i++)
+                arr[i, 0] = arr[i-1, 0] == 1 || text1[i] == text2[0] ? 1 : 0;
+            for(int j=1; j<len2; j++)
+                arr[0,j] = arr[0,j-1] == 1 || text1[0] == text2[j] ? 1 : 0;
+
+            for (int i = 1; i < len1; i++)
+                for (int j = 1; j < len2; j++)
                     if (text1[i] == text2[j])
-                    {
-                        int diag = i > 0 && j > 0 ? arr[i - 1, j - 1] : 0;
-                        arr[i, j] = 1 + diag;
-                    }
+                        arr[i, j] = 1 + arr[i - 1, j - 1];
                     else
-                    {
-                        int left = j > 0 ? arr[i, j - 1] : 0;
-                        int top = i > 0 ? arr[i - 1, j] : 0;
-                        arr[i, j] = left > top ? left : top;
-                    }
+                        arr[i, j] = arr[i, j - 1] > arr[i - 1, j] ? arr[i, j - 1] : arr[i - 1, j];
 
             return arr[len1 - 1, len2 - 1];
         }
