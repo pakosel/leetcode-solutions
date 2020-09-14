c9089afca363d0d3ac9e1281bc61a9bf54202853
pawel <pawel753@gmail.com>
2020-09-14 17:45:43 +0200

    Trie solution for # 720. Longest Word in Dictionary

---
+++ 720. Longest Word in Dictionary/Solution.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace LongestWordInDictionary
+{
+    public class Solution
+    {
+        Trie root = new Trie();
+        public string LongestWord(string[] words)
+        {
+            BuildDict(words);
+            root.IsLeaf = true; //not true but to satisfy DFS while-continue condition
+
+            return DFS(root);
+        }
+
+        private string DFS(Trie node)
+        {
+            string longest = "";
+            Stack<(Trie, string)> stack = new Stack<(Trie, string)>();
+            stack.Push((node, ""));
+
+            while (stack.Count > 0)
+            {
+                var el = stack.Pop();
+                node = el.Item1;
+                var str = el.Item2;
+                if (!node.IsLeaf)
+                    continue;
+                if (str.Length > longest.Length)
+                    longest = str;
+
+                //because of the request: '... smallest lexicographical order ...'
+                for (int i = 25; i >= 0; i--)
+                    if (node.Children[i] != null)
+                        stack.Push((node.Children[i], str + (char)(i + 'a')));
+            }
+            return longest;
+        }
+
+        private void BuildDict(string[] words)
+        {
+            foreach (var word in words)
+            {
+                var node = root;
+                foreach (var c in word)
+                {
+                    int idx = c - 'a';
+                    if (node.Children[idx] == null)
+                        node.Children[idx] = new Trie();
+                    node = node.Children[idx];
+                }
+                node.IsLeaf = true;
+            }
+        }
+    }
+
+    public class Trie
+    {
+        public bool IsLeaf;
+        public Trie[] Children = new Trie[26];
+    }
+}
---
+++ 720. Longest Word in Dictionary/SolutionTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LongestWordInDictionary
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] { new string[] {"w", "wo", "wor", "worl", "world"}, "world" },
+            new object[] { new string[] {"a", "banana", "app", "appl", "ap", "apply", "apple"}, "apple" },
+            new object[] { new string[] {"apple", "banana"}, "" },
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string[] words, string expected)
+        {
+            var sol = new Solution();
+            var res = sol.LongestWord(words);
+
+            Assert.AreEqual(res, expected);
+        }
+    }
+}
