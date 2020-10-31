25a59215c3de6a698207927cc3f271725c67f856
pawel <pawel753@gmail.com>
2020-10-31 16:14:32 +0100

    Interative solutions (Stack and Queue) added for #101

--- 101. Symmetric Tree/Solution.cs
+++ 101. Symmetric Tree/Solution.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace SymmetricTree
 {
-    public class Solution
+    public class Solution_Recursive
     {
         public bool IsSymmetric(TreeNode root) => CheckSymetry(root?.left, root?.right);
 
@@ -19,6 +19,85 @@ namespace SymmetricTree
             return CheckSymetry(left.left, right.right) && CheckSymetry(left.right, right.left);
         }
     }
+
+    public class Solution_Queue
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+            queue.Enqueue(root);
+            while(queue.Count > 0)
+            {
+                var e1 = queue.Dequeue();
+                var e2 = queue.Dequeue();
+                if(e1 == null && e2 == null)
+                    continue;
+                if(e1 == null || e2 == null)
+                    return false;
+                if(e1.val != e2.val)
+                    return false;
+                queue.Enqueue(e1.left);
+                queue.Enqueue(e2.right);
+                queue.Enqueue(e1.right);
+                queue.Enqueue(e2.left);
+            }
+
+            return true;
+        }
+    }
+
+    public class Solution_Stack
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+
+            Stack<TreeNode> leftStack = new Stack<TreeNode>();
+            Stack<TreeNode> rightStack = new Stack<TreeNode>();
+
+            TraverseLeft(root.left, leftStack);
+            TraverseRight(root.right, rightStack);
+
+            while (leftStack.Count > 0 && rightStack.Count > 0)
+            {
+                if (leftStack.Count != rightStack.Count)
+                    return false;
+
+                var leftPop = leftStack.Pop();
+                var rightPop = rightStack.Pop();
+
+                if (leftPop.val != rightPop.val)
+                    return false;
+
+                TraverseLeft(leftPop.right, leftStack);
+                TraverseRight(rightPop.left, rightStack);
+            }
+
+            return leftStack.Count == rightStack.Count;
+        }
+
+        private void TraverseLeft(TreeNode node, Stack<TreeNode> stack)
+        {
+            while (node != null)
+            {
+                stack.Push(node);
+                node = node.left;
+            }
+        }
+
+        private void TraverseRight(TreeNode node, Stack<TreeNode> stack)
+        {
+            while (node != null)
+            {
+                stack.Push(node);
+                node = node.right;
+            }
+        }
+    }
+
 
     public class TreeNode
     {
--- 101. Symmetric Tree/SolutionTest.cs
+++ 101. Symmetric Tree/SolutionTest.cs
@@ -19,13 +19,34 @@ namespace SymmetricTree
             new object[] {new TreeNode(1, new TreeNode(2), null), false },
             new object[] {new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3))), true },
             new object[] {new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3))), false },
+            new object[] {new TreeNode(1, new TreeNode(2, new TreeNode(2)), new TreeNode(2, new TreeNode(2))), false },
         };
 
         [Test]
         [TestCaseSource("testCases")]
-        public void Test_Generic(TreeNode root, bool expected)
+        public void Test_Queue(TreeNode root, bool expected)
         {
-            var sol = new Solution();
+            var sol = new Solution_Queue();
+            var res = sol.IsSymmetric(root);
+
+            Assert.AreEqual(res, expected);
+        }
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Stack(TreeNode root, bool expected)
+        {
+            var sol = new Solution_Stack();
+            var res = sol.IsSymmetric(root);
+
+            Assert.AreEqual(res, expected);
+        }
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Recursive(TreeNode root, bool expected)
+        {
+            var sol = new Solution_Recursive();
             var res = sol.IsSymmetric(root);
 
             Assert.AreEqual(res, expected);
