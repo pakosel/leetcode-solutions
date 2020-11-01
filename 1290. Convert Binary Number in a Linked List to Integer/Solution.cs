ee0b6fce56f265209da3be8ceb5d8b06dee0bfbf
pawel <pawel753@gmail.com>
2020-11-01 15:23:51 +0100

    Solution for #1290. Convert Binary Number in a Linked List to Integer

---
+++ 1290. Convert Binary Number in a Linked List to Integer/Solution.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace ConvertBinaryNumberInLinkedList
+{
+    public class Solution
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            int res = 0;
+            while (head != null)
+            {
+                res *= 2;
+                res += head.val;
+                head = head.next;
+            }
+
+            return res;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ 1290. Convert Binary Number in a Linked List to Integer/SolutionTest.cs
@@ -0,0 +1,44 @@
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConvertBinaryNumberInLinkedList
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCasesStr =
+        {
+            new object[] {"[0]", 0},
+            new object[] {"[0,0]", 0},
+            new object[] {"[1]", 1},
+            new object[] {"[1,0,1]", 5},
+            new object[] {"[1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]", 18880},
+            new object[] {"[1,0,0,1,1,0,1,1,1,1,0,1,0,1,1,0,1,1,1,0,1,0,1,0,1,1,1]", 81704791},
+        };
+
+        [Test]
+        [TestCaseSource("testCasesStr")]
+        public void Test_GenericStr(string arrStr, int expected)
+        {
+            var arr = arrStr.TrimStart('[').TrimEnd(']').Split(",")
+                .Select(s => int.Parse(s)).ToArray();
+
+            Assert.IsTrue(arr.Length > 0);
+
+            ListNode curr = new ListNode(arr[0]);
+            var head = curr;
+            for(int i=1; i<arr.Length; i++)
+            {
+                curr.next = new ListNode(arr[i]);
+                curr = curr.next;
+            }
+
+            var sol = new Solution();
+            var res = sol.GetDecimalValue(head);
+
+            Assert.AreEqual(res, expected);
+        }
+    }
+}
