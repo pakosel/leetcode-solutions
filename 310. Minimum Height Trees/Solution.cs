eabd4420b6717627c03a959b0299e8fac332e8cd
pawel <pawel753@gmail.com>
2020-11-04 16:48:50 +0100

    Initial (BFS) solution for # 310. Minimum Height Trees

---
+++ 310. Minimum Height Trees/Solution.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace MinimumHeightTrees
+{
+    public class Solution
+    {
+        HashSet<int>[] Edges;
+        int min = int.MaxValue;
+
+        public IList<int> FindMinHeightTrees(int n, int[][] edges)
+        {
+            if (n == 1)
+                return new int[1] { 0 };
+            Edges = new HashSet<int>[n];
+
+            foreach (var pair in edges)
+            {
+                AddEdge(pair[0], pair[1]);
+                AddEdge(pair[1], pair[0]);
+            }
+
+            var maxDist = new List<(int, int)>();
+            for (int i = 0; i < n; i++)
+            {
+                var dist = GetMaxDist(i);
+                min = Math.Min(min, dist);
+                maxDist.Add((i, dist));
+            }
+
+            return maxDist.Where(p => p.Item2 == min).Select(p => p.Item1).ToArray();
+        }
+
+        private int GetMaxDist(int vertex)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            int max = 0;
+            Queue<(int, int)> queue = new Queue<(int, int)>();
+            queue.Enqueue((vertex, 0));
+
+            while (queue.Count > 0)
+            {
+                var curr = queue.Dequeue();
+                var v = curr.Item1;
+                var dist = curr.Item2;
+                if (visited.Contains(v))
+                    continue;
+                visited.Add(v);
+                max = Math.Max(max, dist);
+                if (max > min)
+                    break;
+                foreach (int target in Edges[v])
+                    if (!visited.Contains(target))
+                        queue.Enqueue((target, dist + 1));
+            }
+            return max;
+        }
+
+        private void AddEdge(int v1, int v2)
+        {
+            if (Edges[v1] == null)
+                Edges[v1] = new HashSet<int>();
+            Edges[v1].Add(v2);
+        }
+    }
+}
