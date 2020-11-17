7fbaf10befd1916f0a426259be2f63bf99d35e4e
pawel <pawel753@gmail.com>
2020-11-17 21:09:30 +0100

    Solution for # 858. Mirror Reflection

---
+++ 858. Mirror Reflection/Solution.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace MirrorReflection
+{
+    public class Solution
+    {
+        public int MirrorReflection(int p, int q)
+        {
+            if (p == q)
+                return 1;
+            if (q == 0)
+                return 0;
+            if (p % 2 == 1)
+                return q % 2 == 0 ? 0 : 1;
+
+            bool rightSide = true;
+            int sum = q;
+            int overflow = 0;
+            while (sum != 0)
+            {
+                sum += q;
+                if (sum >= p)
+                {
+                    sum %= p;
+                    overflow++;
+                }
+                rightSide = !rightSide;
+            }
+
+            if (!rightSide)
+                return 2;
+
+            return overflow % 2 == 1 ? 1 : 0;
+        }
+    }
+}
---
+++ 858. Mirror Reflection/SolutionTest.cs
@@ -0,0 +1,55 @@
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MirrorReflection
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCasesStr =
+        {
+            new object[] {2, 1, 2},
+            new object[] {2, 2, 1},
+            new object[] {4, 1, 2},
+            new object[] {4, 2, 2},
+            new object[] {4, 3, 2},
+            new object[] {4, 4, 1},
+            new object[] {5, 1, 1},
+            new object[] {5, 2, 0},
+            new object[] {5, 3, 1},
+            new object[] {5, 4, 0},
+            new object[] {5, 5, 1},
+            new object[] {6, 1, 2},
+            new object[] {6, 2, 1},
+            new object[] {6, 3, 2},
+            new object[] {6, 4, 0},
+            new object[] {6, 5, 2},
+            new object[] {6, 6, 1},
+            new object[] {7, 1, 1},
+            new object[] {7, 2, 0},
+            new object[] {7, 3, 1},
+            new object[] {7, 4, 0},
+            new object[] {7, 5, 1},
+            new object[] {7, 6, 0},
+            new object[] {7, 7, 1},
+            new object[] {458, 446, 1},
+            new object[] {458, 12, 0},
+            new object[] {888, 256, 0},
+            new object[] {888, 632, 1},
+            new object[] {888, 248, 1},
+            new object[] {888, 640, 0},
+        };
+
+        [Test]
+        [TestCaseSource("testCasesStr")]
+        public void Test_Memoization(int p, int q, int expected)
+        {
+            var sol = new Solution();
+            var res = sol.MirrorReflection(p, q);
+
+            Assert.AreEqual(res, expected);
+        }
+    }
+}
