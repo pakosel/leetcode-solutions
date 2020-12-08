a47762e90bebaa3aba10e30651cb0932733164f5
pawel <pawel753@gmail.com>
2020-12-08 21:51:51 +0100

    Performance improvements for #1010

--- 1010. Pairs of Songs With Total Durations Divisible by 60/Solution.cs
+++ 1010. Pairs of Songs With Total Durations Divisible by 60/Solution.cs
@@ -7,43 +7,35 @@ namespace PairsOfSongsDivisibleBy60
 {
     public class Solution
     {
-        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
-        int max = 0;
+        Dictionary<int, int> dict = new Dictionary<int, int>();
 
         public int NumPairsDivisibleBy60(int[] time)
         {
             int res = 0;
-            BuildDict(time);
+            
             for (int i = 0; i < time.Length; i++)
-                res += CountMatching(time[i], i);
+            {
+                int t = time[i] % 60;
+                res += CountMatching((60 - t) % 60);
+                AddToDict(t);
+            }
             return res;
         }
 
-        private void BuildDict(int[] time)
+        private void AddToDict(int val)
         {
-            for (int i = 0; i < time.Length; i++)
-            {
-                int t = time[i];
-                if (!dict.ContainsKey(t))
-                    dict.Add(t, new List<int>());
-                dict[t].Add(i);
-                max = Math.Max(max, t);
-            }
+            if (!dict.ContainsKey(val))
+                dict.Add(val, 1);
+            else
+                dict[val]++;
         }
 
-        private int CountMatching(int val, int idx)
+        private int CountMatching(int val)
         {
-            int res = 0;
-            int div = 60;
-            while (div <= 2 * max)
-            {
-                int check = div - val;
-                if (check > 0 && dict.ContainsKey(check))
-                    res += dict[check].Count(i => i < idx);
-                div += 60;
-            }
+            if (dict.ContainsKey(val))
+                return dict[val];
 
-            return res;
+            return 0;
         }
     }
 }
