a11b007b022d56334f398ad4a08743ac0ee16282
pawel <pawel753@gmail.com>
2020-12-08 21:54:13 +0100

    Further performance improvements for #1010

--- 1010. Pairs of Songs With Total Durations Divisible by 60/Solution.cs
+++ 1010. Pairs of Songs With Total Durations Divisible by 60/Solution.cs
@@ -7,7 +7,7 @@ namespace PairsOfSongsDivisibleBy60
 {
     public class Solution
     {
-        Dictionary<int, int> dict = new Dictionary<int, int>();
+        int[] dict = new int[60];
 
         public int NumPairsDivisibleBy60(int[] time)
         {
@@ -16,26 +16,10 @@ namespace PairsOfSongsDivisibleBy60
             for (int i = 0; i < time.Length; i++)
             {
                 int t = time[i] % 60;
-                res += CountMatching((60 - t) % 60);
-                AddToDict(t);
+                res += dict[(60 - t) % 60];
+                dict[t]++;
             }
             return res;
-        }
-
-        private void AddToDict(int val)
-        {
-            if (!dict.ContainsKey(val))
-                dict.Add(val, 1);
-            else
-                dict[val]++;
-        }
-
-        private int CountMatching(int val)
-        {
-            if (dict.ContainsKey(val))
-                return dict[val];
-
-            return 0;
         }
     }
 }
