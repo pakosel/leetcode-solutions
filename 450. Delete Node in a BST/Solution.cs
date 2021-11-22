2ae2b67e9ab65963b9acd60ca2149cd31f01e622
pawel <pawel753@gmail.com>
2021-11-22 22:09:54 +0100

    Cleanup solution for 450. Delete Node in a BST

--- 450. Delete Node in a BST/Solution.cs
+++ 450. Delete Node in a BST/Solution.cs
@@ -2,72 +2,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 using System.Text;
+using Common;
 
 namespace DeleteNodeInBst
 {
-
     public class Solution
     {
-        public TreeNode DeleteNode(TreeNode root, int key)
+        public TreeNode DeleteNode(TreeNode root, int key, TreeNode parent = null)
         {
             if (root == null)
-                return root;
-
-            if (root.val == key)
+                return parent;
+            if (root.val > key)
+                DeleteNode(root.left, key, root);
+            else if (root.val < key)
+                DeleteNode(root.right, key, root);
+            else if (root.val == key)
             {
-                if (root.right == null)
-                    return root.left;
-                var minNode = FindAndExtractMin(root.right);
-                minNode.left = root.left;
-                if (minNode.val == root.right?.val)
-                    return root.right;
-                minNode.right = root.right;
-                return minNode;
-            }
-
-            TreeNode parent = FindParentNodeByChildKey(root, key);
-            if (parent != null)
-            {
-                var replaceLeftChild = parent.left?.val == key;
-                TreeNode nodeToDel;
-                if (replaceLeftChild)
-                    nodeToDel = parent.left;
-                else
-                    nodeToDel = parent.right;
-
-                TreeNode newNode;
-                if(nodeToDel.right != null)
+                var newNode = root.right ?? root.left;
+                if(newNode?.val > key)  //looking for a new node on the right
                 {
-                    newNode = FindAndExtractMin(nodeToDel.right);
-                    newNode.left = nodeToDel.left;
-                    if(newNode.val != nodeToDel.right?.val)
-                        newNode.right = nodeToDel.right;
-                }
-                else
-                    newNode = nodeToDel.left;
+                    newNode = FindMin(newNode);
 
-                if (replaceLeftChild)
-                    parent.left = newNode;
-                else
-                    parent.right = newNode;
+                    newNode.left = root.left;
+                    if (newNode.val != root.right?.val)     //case when FindMin(...) returned one of the child nodes
+                        newNode.right = root.right;
+                }
+                if (parent != null)
+                {
+                    //re-attach the substitute node to parent - depending where the call came from
+                    if (parent.right?.val == key)
+                        parent.right = newNode;
+                    else
+                        parent.left = newNode;
+                    return parent;
+                }
+                return newNode;
             }
 
             return root;
         }
 
-        private TreeNode FindParentNodeByChildKey(TreeNode node, int key)
-        {
-            if (node == null)
-                return null;
-            if (node.left?.val == key || node.right?.val == key)
-                return node;
-            if (node.val > key)
-                return FindParentNodeByChildKey(node.left, key);
-
-            return FindParentNodeByChildKey(node.right, key);
-        }
-
-        private TreeNode FindAndExtractMin(TreeNode node)
+        private TreeNode FindMin(TreeNode node)
         {
             TreeNode parent = null;
             while (node?.left != null)
@@ -75,22 +50,10 @@ namespace DeleteNodeInBst
                 parent = node;
                 node = node.left;
             }
-            if (parent != null) //extract and replace right subtree
-                parent.left = node.right;
-
+            if (parent != null)
+                //switch any child nodes to parent
+                parent.left = node?.right;
             return node;
-        }
-    }
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
         }
     }
 }
--- 450. Delete Node in a BST/SolutionTest.cs
+++ 450. Delete Node in a BST/SolutionTest.cs
@@ -2,6 +2,7 @@ using System.Text;
 using NUnit.Framework;
 using System.Linq;
 using System.Collections.Generic;
+using Common;
 
 namespace DeleteNodeInBst
 {
@@ -10,36 +11,33 @@ namespace DeleteNodeInBst
     {
         private static readonly object[] testCases =
         {
-            new object[] { new TreeNode(3, new TreeNode(2), new TreeNode(5, new TreeNode(4), new TreeNode(10, new TreeNode(8, new TreeNode(7)), new TreeNode(15)))), 5, new TreeNode(3, new TreeNode(2), new TreeNode(7, new TreeNode(4), new TreeNode(10, new TreeNode(8), new TreeNode(15)))) },
-            new object[] { new TreeNode(50, new TreeNode(30, null, new TreeNode(40)), new TreeNode(70, new TreeNode(60), new TreeNode(80))), 50, new TreeNode(60, new TreeNode(30, null, new TreeNode(40)), new TreeNode(70, null, new TreeNode(80))) },
-            new object[] { new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(6, null, new TreeNode(7))), 3, new TreeNode(5, new TreeNode(4, new TreeNode(2)), new TreeNode(6, null, new TreeNode(7))) },
-            new object[] { new TreeNode(4, null, new TreeNode(7, new TreeNode(6, new TreeNode(5)), new TreeNode(8, null, new TreeNode(9)))), 7, new TreeNode(4, null, new TreeNode(8, new TreeNode(6, new TreeNode(5)), new TreeNode(9))) },
-            new object[] { new TreeNode(3, new TreeNode(2, new TreeNode(1)), new TreeNode(4)), 2, new TreeNode(3, new TreeNode(1), new TreeNode(4)) },
-            new object[] { new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4)), 1, new TreeNode(3, new TreeNode(2), new TreeNode(4)) },
+            new object[] {"[]", 0, "[]"},
+            new object[] {"[5,3,6,2,4,null,7]", 3, "[5,4,6,2,null,null,7]"},
+            new object[] {"[5,3,6,2,4,null,7]", 0, "[5,3,6,2,4,null,7]"},
+            new object[] {"[5,3,6,2,4,null,7]", 7, "[5,3,6,2,4]"},
+            new object[] {"[3,2,5,null,null,4,10,null,null,8,15,7]", 5, "[3,2,7,null,null,4,10,null,null,8,15]"},
+            new object[] {"[50,30,70,null,40,60,80]", 50, "[60,30,70,null,40,null,80]"},
+            new object[] {"[4,null,7,6,8,5,null,null,9]", 7, "[4,null,8,6,9,5]"},
+            new object[] {"[3,2,4,1]", 2, "[3,1,4]"},
+            new object[] {"[3,1,4,null,2]", 1, "[3,2,4]"},
+            new object[] {"[2,0,33,null,1,25,40,null,null,11,31,34,45,10,18,29,32,null,36,43,46,4,null,12,24,26,30,null,null,35,39,42,44,null,48,3,9,null,14,22,null,null,27,null,null,null,null,38,null,41,null,null,null,47,49,null,null,5,null,13,15,21,23,null,28,37,null,null,null,null,null,null,null,null,8,null,null,null,17,19,null,null,null,null,null,null,null,7,null,16,null,null,20,6]", 33, "[2,0,34,null,1,25,40,null,null,11,31,36,45,10,18,29,32,35,39,43,46,4,null,12,24,26,30,null,null,null,null,38,null,42,44,null,48,3,9,null,14,22,null,null,27,null,null,37,null,41,null,null,null,47,49,null,null,5,null,13,15,21,23,null,28,null,null,null,null,null,null,null,null,null,8,null,null,null,17,19,null,null,null,null,null,7,null,16,null,null,20,6]"},
+            new object[] {"[5,3,6,2,4,null,null,1]", 3, "[5,4,6,2,null,null,null,1]"},
+            new object[] {"[27,17,33,15,26,32,45,7,16,23,null,31,null,40,49,5,14,null,null,21,25,29,null,34,41,48,null,2,6,10,null,20,22,24,null,28,30,null,39,null,42,46,null,1,4,null,null,9,11,18,null,null,null,null,null,null,null,null,null,37,null,null,44,null,47,0,null,3,null,8,null,null,13,null,19,35,38,43,null,null,null,null,null,null,null,null,null,12,null,null,null,null,36]", 49, "[27,17,33,15,26,32,45,7,16,23,null,31,null,40,48,5,14,null,null,21,25,29,null,34,41,46,null,2,6,10,null,20,22,24,null,28,30,null,39,null,42,null,47,1,4,null,null,9,11,18,null,null,null,null,null,null,null,null,null,37,null,null,44,null,null,0,null,3,null,8,null,null,13,null,19,35,38,43,null,null,null,null,null,null,null,12,null,null,null,null,36]"},
+            new object[] {"[2,1]", 1, "[2]"},
         };
 
         [Test]
         [TestCaseSource("testCases")]
-        public void Test_Generic(TreeNode input, int key, TreeNode expected)
+        public void Test_Generic(string inputStr, int key, string expectedStr)
         {
+            var root = TreeNodeHelper.BuildTree(inputStr);
+
             var sol = new Solution();
-            var res = sol.DeleteNode(input, key);
+            var res = sol.DeleteNode(root, key);
 
-            Assert.IsTrue(CompareTreeNode(res, expected));
-        }
+            var expected = TreeNodeHelper.BuildTree(expectedStr);
 
-        private bool CompareTreeNode(TreeNode node1, TreeNode node2)
-        {
-            if(node1 == null && node2 == null)
-                return true;
-            if(node1 != null && node2 != null)
-            {
-                return node1.val == node2.val 
-                        && CompareTreeNode(node1.left, node2.left) 
-                        && CompareTreeNode(node1.right, node2.right);
-            }
-            
-            return false;
+            Assert.IsTrue(TreeNodeHelper.CompareTreeNode(res, expected));
         }
     }
 }
