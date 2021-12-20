f4d2621c2161a3c616b894469a87e27c00ec3308
pawel <pawel753@gmail.com>
2021-12-20 17:08:41 +0100

    Solution for 1091. Shortest Path in Binary Matrix

---
+++ 1091. Shortest Path in Binary Matrix/Solution.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace ShortestPathInBinaryMatrix
+{
+    public class Solution
+    {
+        public int ShortestPathBinaryMatrix(int[][] grid)
+        {
+            int n = grid.Length;
+            if (grid[n - 1][n - 1] != 0)
+                return -1;
+
+            var queue = new Queue<(int x, int y, int len)>();
+            var min = int.MaxValue;
+
+            queue.Enqueue((0, 0, 1));
+            while (queue.Count > 0)
+            {
+                var q = queue.Dequeue();
+                if (q.x < 0 || q.x >= n || q.y < 0 || q.y >= n)
+                    continue;
+                if (q.x == n - 1 && q.y == n - 1)
+                    min = Math.Min(min, q.len);
+
+                var val = grid[q.x][q.y];
+                if (val == 1)
+                    continue;
+                if (val == 0 || val > q.len)
+                {
+                    grid[q.x][q.y] = q.len;
+                    queue.Enqueue((q.x, q.y + 1, q.len + 1));
+                    queue.Enqueue((q.x, q.y - 1, q.len + 1));
+                    queue.Enqueue((q.x + 1, q.y, q.len + 1));
+                    queue.Enqueue((q.x - 1, q.y, q.len + 1));
+                    queue.Enqueue((q.x + 1, q.y + 1, q.len + 1));
+                    queue.Enqueue((q.x + 1, q.y - 1, q.len + 1));
+                    queue.Enqueue((q.x - 1, q.y + 1, q.len + 1));
+                    queue.Enqueue((q.x - 1, q.y - 1, q.len + 1));
+                }
+            }
+
+            return min != int.MaxValue ? min : -1;
+        }
+    }
+}
---
+++ 1091. Shortest Path in Binary Matrix/SolutionTest.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using Common;
+
+namespace ShortestPathInBinaryMatrix
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] {"[[0,1],[1,0]]", 2},
+            new object[] {"[[0,0,0],[1,1,0],[1,1,0]]", 4},
+            new object[] {"[[1,0,0],[1,1,0],[1,1,0]]", -1},
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string gridStr, int expected)
+        {
+            var grid = ArrayHelper.MatrixFromString(gridStr);
+
+            var sol = new Solution();
+            var res = sol.ShortestPathBinaryMatrix(grid);
+
+            Assert.AreEqual(res, expected);
+        }
+    }
+}
