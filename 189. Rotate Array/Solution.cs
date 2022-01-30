135f921f6bf8fdc9e3a81164779a5251d54bb932
pawel <pawel753@gmail.com>
2022-01-30 14:00:22 +0100

    3x solution for 189. Rotate Array

---
+++ 189. Rotate Array/Solution.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Common;
+
+namespace RotateArray
+{
+    public class Solution_Basic
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            var len = nums.Length;
+            var res = new int[len];
+            k = k % len;
+            for (int i = 0; i < len; i++)
+                res[(i + k) % len] = nums[i];
+            Array.Copy(res, nums, len);
+        }
+    }
+    public class Solution_O_k
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            var len = nums.Length;
+            k = k % len;
+            if (k == 0)
+                return;
+            var temp = new int[k];
+            int i = 0;
+            int j = 0;
+            while (i < k)
+                temp[i] = nums[i++];
+            while (i != k - 1)
+            {
+                (nums[i], temp[j]) = (temp[j], nums[i]);
+                i = (i + 1) % len;
+                j = (j + 1) % k;
+            }
+            nums[k - 1] = temp[j];
+        }
+    }
+
+    public class Solution_O_1
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            var len = nums.Length;
+            k = k % len;
+            if (k == 0)
+                return;
+            var temp = 0;
+            var i = 0;
+            var gcd = GCD(len, k);
+            for (int j = 0; j < gcd; j++)
+            {
+                temp = nums[j];
+                i = j + k;
+                while (i != j)
+                {
+                    (nums[i], temp) = (temp, nums[i]);
+                    i = (i + k) % len;
+                }
+                nums[i] = temp;
+            }
+        }
+
+        private static int GCD(int a, int b)
+        {
+            while (a != 0 && b != 0)
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+
+            return a | b;
+        }
+    }
+}
---
+++ 189. Rotate Array/SolutionTest.cs
@@ -0,0 +1,64 @@
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using Common;
+
+namespace RotateArray
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] {"[1]", 0, "[1]"},
+            new object[] {"[1]", 2, "[1]"},
+            new object[] {"[1]", 1234, "[1]"},
+            new object[] {"[1,2,3,4,5,6,7]", 0, "[1,2,3,4,5,6,7]"},
+            new object[] {"[1,2,3,4,5,6,7]", 37, "[6,7,1,2,3,4,5]"},
+            new object[] {"[1,2,3,4,5,6,7]", 3, "[5,6,7,1,2,3,4]"},
+            new object[] {"[-1,-100,3,99]", 2, "[3,99,-1,-100]"},
+            new object[] {"[1,2,3,4,5,6]", 2, "[5,6,1,2,3,4]"},
+            new object[] {"[1,2,3,4,5,6]", 4, "[3,4,5,6,1,2]"},
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string numsStr, int k, string expectedStr)
+        {
+            var nums = ArrayHelper.ArrayFromString(numsStr);
+            var expected = ArrayHelper.ArrayFromString(expectedStr);
+
+            var sol = new Solution_Basic();
+            sol.Rotate(nums, k);
+
+            CollectionAssert.AreEqual(nums, expected);
+        }
+        
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_O_k(string numsStr, int k, string expectedStr)
+        {
+            var nums = ArrayHelper.ArrayFromString(numsStr);
+            var expected = ArrayHelper.ArrayFromString(expectedStr);
+
+            var sol = new Solution_O_k();
+            sol.Rotate(nums, k);
+
+            CollectionAssert.AreEqual(nums, expected);
+        }
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_O_1(string numsStr, int k, string expectedStr)
+        {
+            var nums = ArrayHelper.ArrayFromString(numsStr);
+            var expected = ArrayHelper.ArrayFromString(expectedStr);
+
+            var sol = new Solution_O_1();
+            sol.Rotate(nums, k);
+
+            CollectionAssert.AreEqual(nums, expected);
+        }
+    }
+}
