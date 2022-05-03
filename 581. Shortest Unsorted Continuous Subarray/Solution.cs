349fa715283872ee7a9ad3762ad7445366581ff5
pawel <pawel753@gmail.com>
2022-05-03 20:06:29 +0200

    Fixed solution for 581. Shortest Unsorted Continuous Subarray

--- 581. Shortest Unsorted Continuous Subarray/Solution.cs
+++ 581. Shortest Unsorted Continuous Subarray/Solution.cs
@@ -11,30 +11,30 @@ namespace ShortestUnsortedContinuousSubarray
         {
             var len = nums.Length;
             var left = 0;
-            var isSet = true;
+            var increasing = true;
             for (int i = 1; i < len; i++)
-                if (!isSet && nums[i] >= nums[left])
+                if (increasing && nums[i] >= nums[left])
                     left = i;
                 else
                 {
                     while (left >= 0 && nums[left] > nums[i])
                         left--;
-                    isSet = true;
+                    increasing = false;     //from now on - we can only decrement left pointer
                 }
 
             if (left == len - 1)
                 return 0;
 
-            isSet = false;
+            var decreasing = true;
             int right = len - 1;
             for (int i = len - 2; i > left; i--)
-                if (!isSet && nums[right] >= nums[i])
+                if (decreasing && nums[right] >= nums[i])
                     right = i;
                 else
                 {
                     while (right < len && nums[right] < nums[i])
                         right++;
-                    isSet = true;
+                    decreasing = false;     //from now on - we can only increment right pointer
                 }
 
             left++;
