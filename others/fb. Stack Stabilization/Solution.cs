8624e3f0e2273bd63ff03846ea42b81b57b0274b
pawel <pawel753@gmail.com>
2022-05-24 10:03:50 +0200

    Cleanup and add some comments

--- others/fb. Stack Stabilization/Solution.cs
+++ others/fb. Stack Stabilization/Solution.cs
@@ -13,21 +13,21 @@ namespace StackStabilization
             var min = Math.Max(0, R.Min() - N);
             var max = R.Max();
             var size = max - min + N;
-            var arr = Enumerable.Range(0, N).Select(_ => new long[size]).ToArray();
-            arr[0][0] = int.MaxValue;
+            var dp = Enumerable.Range(0, N).Select(_ => new long[size]).ToArray();
 
-            for(int i=0; i<N; i++)
-                for(int j=i+1; j<size; j++)
+            for (int i = 0; i < N; i++)
+                for (int j = i + 1; j < size; j++)
                 {
-                    var val = min+j;
-                    var prev = (i > 0 ? arr[i-1][j-1] : 0);
-                    arr[i][j] = prev + (val < R[i] ? (R[i] - val)*B : (val - R[i])*A);
-                    if(j > i+1)
-                        arr[i][j] = Math.Min(arr[i][j-1], arr[i][j]);
+                    var prev = (i > 0 ? dp[i - 1][j - 1] : 0);  //for 0-th row no pervious MIN value
+                    var val = min + j;
+                    var curr = (val < R[i] ? (R[i] - val) * B : (val - R[i]) * A);
+                    dp[i][j] = prev + curr;
+                    if (j > i + 1)  //first value in a row has to be calculated
+                        dp[i][j] = Math.Min(dp[i][j - 1], dp[i][j]);
                 }
             // foreach(var row in arr)
             //     Console.WriteLine(string.Join('\t', row));
-            return arr[N-1][size-1];
+            return dp[N - 1][size - 1];
         }
     }
 }
