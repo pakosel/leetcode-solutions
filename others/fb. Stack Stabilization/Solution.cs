4d646901c6d7888215a96ac958225cfdcacfc14b
pawel <pawel753@gmail.com>
2022-05-24 11:55:29 +0200

    Memory usage optimization

--- others/fb. Stack Stabilization/Solution.cs
+++ others/fb. Stack Stabilization/Solution.cs
@@ -13,21 +13,23 @@ namespace StackStabilization
             var min = Math.Max(0, R.Min() - N);
             var max = R.Max();
             var size = max - min + N;
-            var dp = Enumerable.Range(0, N).Select(_ => new long[size]).ToArray();
-
+            var dp = new long[size];
+            long prevRowDiag = 0;
             for (int i = 0; i < N; i++)
+            {
+                prevRowDiag = dp[i];
                 for (int j = i + 1; j < size; j++)
                 {
-                    var prev = (i > 0 ? dp[i - 1][j - 1] : 0);  //for 0-th row no pervious MIN value
-                    var val = min + j;
-                    var curr = (val < R[i] ? (R[i] - val) * B : (val - R[i]) * A);
-                    dp[i][j] = prev + curr;
+                    var prev = (i > 0 ? prevRowDiag : 0);  //for 0-th row no pervious MIN value
+                    long val = min + j;
+                    long curr = (val < R[i] ? (R[i] - val) * B : (val - R[i]) * A);
+                    prevRowDiag = dp[j];
+                    dp[j] = prev + curr;
                     if (j > i + 1)  //first value in a row has to be calculated
-                        dp[i][j] = Math.Min(dp[i][j - 1], dp[i][j]);
+                        dp[j] = Math.Min(dp[j - 1], dp[j]);
                 }
-            // foreach(var row in arr)
-            //     Console.WriteLine(string.Join('\t', row));
-            return dp[N - 1][size - 1];
+            }
+            return dp[size - 1];
         }
     }
 }
