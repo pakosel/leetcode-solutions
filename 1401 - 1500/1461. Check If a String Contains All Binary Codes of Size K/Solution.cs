801ecd94c2efbadd7e0e830d129344252d8bb759
pawel <pawel753@gmail.com>
2022-05-31 17:59:36 +0200

    Add some comments

--- 1401 - 1500/1461. Check If a String Contains All Binary Codes of Size K/Solution.cs
+++ 1401 - 1500/1461. Check If a String Contains All Binary Codes of Size K/Solution.cs
@@ -11,7 +11,7 @@ namespace CheckIfStringContainsAllBinaryCodesOfSizeK
         public bool HasAllCodes(string s, int k)
         {
             var set = new HashSet<int>();
-            var tgt = (1 << k);
+            var tgt = (1 << k);     //number of all different values consisting of k bits
             var mask = (1 << k) - 1;
             var num = 0;
             int i = -1;
@@ -19,8 +19,8 @@ namespace CheckIfStringContainsAllBinaryCodesOfSizeK
             while(++i < s.Length && set.Count < tgt)
             {
                 num = (num << 1) + (s[i] - '0');
-                num &= mask;
-                if(i >= k-1)
+                num &= mask;    //turn k-th bit OFF (we're interested in values of length k-1 )
+                if(i >= k-1)    //save num if we already have enough bits
                     set.Add(num);
             }
 
