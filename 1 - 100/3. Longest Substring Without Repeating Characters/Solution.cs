55b58ad5cda1301c2fa480dd41cb37f0ecbd9000
pawel <pawel753@gmail.com>
2022-06-10 09:07:06 +0200

    Cleanup solution for 3. Longest Substring Without Repeating Characters

--- 1 - 100/3. Longest Substring Without Repeating Characters/Solution.cs
+++ 1 - 100/3. Longest Substring Without Repeating Characters/Solution.cs
@@ -1,8 +1,28 @@
+using System;
 using System.Collections.Generic;
 
 namespace LongestSubstringWithoutRepeatingCharacters
 {
     public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var max = 0;
+            var arr = new bool[126];
+            var left = 0;
+            var right = -1;
+            while (++right < s.Length && max < 95)
+            {
+                var c = s[right] - ' ';
+                while (arr[c] && left < right)
+                    arr[s[left++] - ' '] = false;
+                arr[c] = true;
+                max = Math.Max(max, right - left + 1);
+            }
+            return max;
+        }
+    }
+    public class Solution2020
     {
         public int LengthOfLongestSubstring(string s)
         {
--- 1 - 100/3. Longest Substring Without Repeating Characters/SolutionTest.cs
+++ 1 - 100/3. Longest Substring Without Repeating Characters/SolutionTest.cs
@@ -1,35 +1,43 @@
+using System.Text;
 using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using Common;
 
 namespace LongestSubstringWithoutRepeatingCharacters
 {
     [TestFixture]
     public class SolutionTest
     {
-        [Test]
-        public void Test_Simple()
+        private static readonly object[] testCases =
         {
-            var solution = new Solution();
-            var ret = solution.LengthOfLongestSubstring("abcabcbb");
+            new object[] {"a", 1},
+            new object[] {"abcabcbb", 3},
+            new object[] {"abcddcba", 4},
+            new object[] {"axwabcdefgahij", 10},
+            new object[] {"bbbbb", 1},
+            new object[] {"pwwkew", 3},
+            new object[] {"abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+`~[]{};':,./<>?\"\\|", 95},
+        };
 
-            Assert.AreEqual(ret, 3);
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string s, int expected)
+        {
+            var sol = new Solution();
+            var res = sol.LengthOfLongestSubstring(s);
+
+            Assert.AreEqual(expected, res);
         }
-
+        
         [Test]
-        public void Test_Tricky()
+        [TestCaseSource("testCases")]
+        public void Test_Old(string s, int expected)
         {
-            var solution = new Solution();
-            var ret = solution.LengthOfLongestSubstring("abcddcba");
+            var sol = new Solution2020();
+            var res = sol.LengthOfLongestSubstring(s);
 
-            Assert.AreEqual(ret, 4);
-        }
-
-        [Test]
-        public void Test_Complex()
-        {
-            var solution = new Solution();
-            var ret = solution.LengthOfLongestSubstring("axwabcdefgahij");
-
-            Assert.AreEqual(ret, 10);
+            Assert.AreEqual(expected, res);
         }
     }
 }
