3ea597e1341e9587f9cf7a1f5c61674bc5c247c8
pawel <pawel753@gmail.com>
2022-07-13 22:07:45 +0200

    Cleanup and refactoring for 102. Binary Tree Level Order Traversal

--- 101 - 200/102. Binary Tree Level Order Traversal/Solution.cs
+++ 101 - 200/102. Binary Tree Level Order Traversal/Solution.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 using System.Text;
+using Common;
 
 namespace BinaryTreeLevelOrderTraversal
 {
@@ -10,39 +11,25 @@ namespace BinaryTreeLevelOrderTraversal
         public IList<IList<int>> LevelOrder(TreeNode root)
         {
             IList<IList<int>> res = new List<IList<int>>();
-            var queue = new Queue<Tuple<TreeNode, int>>();
-            if(root != null)
-                queue.Enqueue(new Tuple<TreeNode, int>(root, 0));
+            var queue = new Queue<TreeNode>();
+            if (root != null)
+                queue.Enqueue(root);
 
             while (queue.Count() > 0)
             {
-                var nodeKvp = queue.Dequeue();
-                var node = nodeKvp.Item1;
-                var level = nodeKvp.Item2;
-                if (res.Count() > level)
-                    res[level].Add(node.val);
-                else
-                    res.Add(new List<int>() { node.val });
-
-                if (node.left != null)
-                    queue.Enqueue(new Tuple<TreeNode, int>(node.left, level + 1));
-                if (node.right != null)
-                    queue.Enqueue(new Tuple<TreeNode, int>(node.right, level + 1));
+                var cnt = queue.Count;
+                var arr = new int[cnt];
+                for (int i = 0; i < cnt; i++)
+                {
+                    var node = queue.Dequeue();
+                    arr[i] = node.val;
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+                res.Add(arr);
             }
 
             return res;
-        }
-    }
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
         }
     }
 }
--- 101 - 200/102. Binary Tree Level Order Traversal/SolutionTest.cs
+++ 101 - 200/102. Binary Tree Level Order Traversal/SolutionTest.cs
@@ -2,6 +2,8 @@ using System.Text;
 using NUnit.Framework;
 using System.Linq;
 using System.Collections.Generic;
+using Common;
+
 
 namespace BinaryTreeLevelOrderTraversal
 {
@@ -10,14 +12,19 @@ namespace BinaryTreeLevelOrderTraversal
     {
         private static readonly object[] testCases =
         {
-            new object[] {new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))), new List<IList<int>> {new int[] {3}, new int[] {9, 20}, new int[] {15,7}} },
-            new object[] {new TreeNode(0, new TreeNode(2, new TreeNode(1, new TreeNode(5), new TreeNode(1))), new TreeNode(4, new TreeNode(3, null, new TreeNode(6)), new TreeNode(-1, null, new TreeNode(8)))), new List<IList<int>> {new int[] {0}, new int[] {2,4}, new int[] {1,3,-1}, new int[] {5,1,6,8}} },
+            new object[] {"[]", "[]"},
+            new object[] {"[1]", "[[1]]"},
+            new object[] {"[3,9,20,null,null,15,7]", "[[3],[9,20],[15,7]]"},
+            new object[] {"[0,2,4,1,null,3,-1,5,1,null,6,null,8]", "[[0],[2,4],[1,3,-1],[5,1,6,8]]"},
         };
 
         [Test]
         [TestCaseSource("testCases")]
-        public void Test_Generic(TreeNode root, IList<IList<int>> expected)
+        public void Test_Generic(string treeStr, string expectedStr)
         {
+            var root = TreeNodeHelper.BuildTree(treeStr);
+            var expected = ArrayHelper.MatrixFromString<int>(expectedStr);
+
             var sol = new Solution();
             var res = sol.LevelOrder(root);
 
