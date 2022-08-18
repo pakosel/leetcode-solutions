1c7116b2d0b7f00df3d9d4e5a662bc788001e74f
pawel <pawel753@gmail.com>
2022-08-18 20:37:21 +0200

    Performance improvement of existing solution

--- 1 - 100/30. Substring with Concatenation of All Words/Solution.cs
+++ 1 - 100/30. Substring with Concatenation of All Words/Solution.cs
@@ -5,7 +5,63 @@ using System.Text;
 
 namespace SubstringConcatenationAllWords
 {
+    //improved version of _TLE solution
     public class Solution
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            int len = s.Length;
+            int wordLen = words[0].Length;
+            int concatLen = wordLen * words.Length;
+
+            List<int> res = new List<int>();
+            if (len < concatLen)
+                return res;
+            var allWords = new Dictionary<string, int>();
+            foreach (var w in words)
+                if (allWords.ContainsKey(w))
+                    allWords[w]++;
+                else
+                    allWords.Add(w, 1);
+
+            var wordsList = new Dictionary<string, int>(allWords);
+            var wordsCnt = 0;
+
+            int left = 0;
+            int right = wordLen;
+
+            while (left < len && right < len + 1)
+            {
+                var sub = s.Substring(right - wordLen, wordLen);
+                if (wordsList.ContainsKey(sub) && wordsList[sub] > 0)
+                {
+                    wordsList[sub]--;
+                    wordsCnt++;
+                    if (wordsCnt == words.Length)
+                    {
+                        res.Add(left);
+                        left++;
+                        right = left + wordLen;
+                        wordsList = new Dictionary<string, int>(allWords);
+                        wordsCnt = 0;
+                    }
+                    else
+                        right += wordLen;
+                }
+                else
+                {
+                    left++;
+                    right = left + wordLen;
+                    wordsList = new Dictionary<string, int>(allWords);
+                    wordsCnt = 0;
+                }
+            }
+
+            return res;
+        }
+    }
+
+    public class Solution_TLE
     {
         public IList<int> FindSubstring(string s, string[] words)
         {
@@ -22,13 +78,13 @@ namespace SubstringConcatenationAllWords
             int left = 0;
             int right = wordLen;
 
-            while(left < len && right < len+1)
+            while (left < len && right < len + 1)
             {
                 var sub = s.Substring(right - wordLen, wordLen);
-                if(wordsList.Contains(sub))
+                if (wordsList.Contains(sub))
                 {
                     wordsList.Remove(sub);
-                    if(wordsList.Count == 0)
+                    if (wordsList.Count == 0)
                     {
                         res.Add(left);
                         left++;
