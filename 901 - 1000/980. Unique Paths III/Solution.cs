ef15bda499fda9d442e779204914c5eb7c4d917a
pawel <pawel753@gmail.com>
2022-12-31 09:58:57 +0100

    Own Point implementation + cleanup

--- 901 - 1000/980. Unique Paths III/Solution.cs
+++ 901 - 1000/980. Unique Paths III/Solution.cs
@@ -11,51 +11,50 @@ namespace UniquePathsIII
     {
         public int UniquePathsIII(int[][] grid)
         {
-            var m = grid.Length;
-            int n = grid[0].Length;
-            (int x, int y) start = (0, 0);
-            (int x, int y) end = (0, 0);
-            var set = new HashSet<(int, int)>();
+            Point start = new(0, 0);
+            Point end = new(0, 0);
+            var moves = new Point[] {new(1,0), new(0,1), new(-1,0), new(0,-1)};
+            var set = new HashSet<Point>();
             ScanGrid();
 
             var res = 0;
-            var queue = new Queue<(int x, int y, HashSet<(int, int)> path)>();
-            queue.Enqueue((start.x, start.y, new HashSet<(int, int)>()));
+            var queue = new Queue<(Point point, HashSet<Point> path)>();
+            queue.Enqueue((start, new HashSet<Point>()));
+
             while (queue.Count > 0)
             {
                 var curr = queue.Dequeue();
-                if (curr.path.Contains((curr.x, curr.y)) || !set.Contains((curr.x, curr.y)))
+                if (curr.path.Contains(curr.point) || !set.Contains(curr.point))
                     continue;
-                curr.path.Add((curr.x, curr.y));
-                if ((curr.x, curr.y) == end)
+
+                curr.path.Add(curr.point);
+                if (curr.point == end)
                 {
                     if (curr.path.Count == set.Count)
                         res++;
                     continue;
                 }
-                queue.Enqueue((curr.x + 1, curr.y, new HashSet<(int, int)>(curr.path)));
-                queue.Enqueue((curr.x - 1, curr.y, new HashSet<(int, int)>(curr.path)));
-                queue.Enqueue((curr.x, curr.y + 1, new HashSet<(int, int)>(curr.path)));
-                queue.Enqueue((curr.x, curr.y - 1, new HashSet<(int, int)>(curr.path)));
+                foreach(var m in moves)
+                    queue.Enqueue((curr.point + m, new HashSet<Point>(curr.path)));
             }
             return res;
 
             void ScanGrid()
             {
-                for (int i = 0; i < m; i++)
-                    for (int j = 0; j < n; j++)
+                for (int i = 0; i < grid.Length; i++)
+                    for (int j = 0; j < grid[0].Length; j++)
                         switch (grid[i][j])
                         {
                             case 0:
-                                set.Add((i, j));
+                                set.Add(new(i, j));
                                 break;
                             case 1:
-                                start = (i, j);
-                                set.Add((i, j));
+                                start = new(i, j);
+                                set.Add(new(i, j));
                                 break;
                             case 2:
-                                end = (i, j);
-                                set.Add((i, j));
+                                end = new(i, j);
+                                set.Add(new(i, j));
                                 break;
                             default:
                                 break;
