556d1935488a46a6f9bd0a9d949b6d21a507938f
pawel <pawel753@gmail.com>
2023-03-23 20:37:39 +0100

    New solution for 1319.

--- 1301 - 1400/1319. Number of Operations to Make Network Connected/Solution.cs
+++ 1301 - 1400/1319. Number of Operations to Make Network Connected/Solution.cs
@@ -6,6 +6,54 @@ using Common;
 
 namespace NumberOfOperationsToMakeNetworkConnected
 {
+    public class Solution_2023
+    {
+        public int MakeConnected(int n, int[][] connections)
+        {
+            var graph = BuildGraph();
+            var visited = new HashSet<int>();
+            var partitions = 0;
+
+            for (int i = 0; i < n; i++)
+                if (!visited.Contains(i))
+                {
+                    partitions++;
+                    if (graph.ContainsKey(i))
+                        Dfs(i);
+                }
+            
+            if (connections.Length >= n-1)
+                return partitions - 1;
+
+            return -1;
+
+            void Dfs(int node)
+            {
+                if (visited.Contains(node))
+                    return;
+                visited.Add(node);
+                foreach (var n in graph[node])
+                    if (!visited.Contains(n))
+                        Dfs(n);
+            }
+
+            Dictionary<int, List<int>> BuildGraph()
+            {
+                var res = new Dictionary<int, List<int>>();
+                foreach (var conn in connections)
+                {
+                    if (!res.ContainsKey(conn[0]))
+                        res.Add(conn[0], new List<int>());
+                    if (!res.ContainsKey(conn[1]))
+                        res.Add(conn[1], new List<int>());
+                    res[conn[0]].Add(conn[1]);
+                    res[conn[1]].Add(conn[0]);
+                }
+                return res;
+            }
+        }
+    }
+
     public class Solution
     {
         public int MakeConnected(int n, int[][] connections)
