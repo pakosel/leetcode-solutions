c736e0e8e81d787ec8f31f568fa26fc8a4119c25
pawel <pawel753@gmail.com>
2023-04-24 07:26:49 +0200

    Improve solution 1046.

--- 1001 - 1100/1046. Last Stone Weight/Solution.cs
+++ 1001 - 1100/1046. Last Stone Weight/Solution.cs
@@ -9,15 +9,16 @@ namespace LastStoneWeight
     {
         public int LastStoneWeight(int[] stones)
         {
-            var pq = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+            var pq = new PriorityQueue<int, int>();
             foreach (var s in stones)
-                pq.Enqueue(s, s);
+                pq.Enqueue(s, -s);
             while (pq.Count > 1)
             {
                 var e1 = pq.Dequeue();
                 var e2 = pq.Dequeue();
-                if (e1 != e2)
-                    pq.Enqueue(e1 - e2, e1 - e2);
+                var crash = e1 - e2;
+                if (crash != 0)
+                    pq.Enqueue(crash, -crash);
             }
             return pq.Count > 0 ? pq.Dequeue() : 0;
         }
