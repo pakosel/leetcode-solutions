472031471fded66df44985514f66e29c3edaa6ef
pawel <pawel753@gmail.com>
2024-02-04 15:11:10 +0100

    Solution to 1043.

---
+++ 1001 - 1100/1043. Partition Array for Maximum Sum/SolutionTest.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using Common;
+
+namespace PartitionArrayForMaximumSum
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static readonly object[] testCases =
+        {
+            new object[] {"[1,15,7,9,2,5,10]", 3, 84},
+            new object[] {"[1,4,1,5,7,3,6,1,9,9,3]", 4, 83},
+            new object[] {"[1]", 1, 1},
+        };
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string arrStr, int k, int expected)
+        {
+            var arr = ArrayHelper.ArrayFromString<int>(arrStr);
+
+            var sol = new Solution();
+            var res = sol.MaxSumAfterPartitioning(arr, k);
+            
+            Assert.AreEqual(expected, res);
+        }
+    }
+}
