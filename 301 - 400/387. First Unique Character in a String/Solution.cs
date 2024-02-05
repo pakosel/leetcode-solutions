b5009b13f196986476f2c29f035591bbdb77a01d
pawel <pawel753@gmail.com>
2024-02-05 16:46:47 +0100

    New solution to 387.

--- 301 - 400/387. First Unique Character in a String/Solution.cs
+++ 301 - 400/387. First Unique Character in a String/Solution.cs
@@ -5,6 +5,19 @@ using System.Text;
 
 namespace FirstUniqueCharacterInString
 {
+    public class Solution2023
+    {
+        public int FirstUniqChar(string s)
+        {
+            var arr = new int[26];
+            foreach (var c in s)
+                arr[c - 'a']++;
+            for (int i = 0; i < s.Length; i++)
+                if (arr[s[i] - 'a'] == 1)
+                    return i;
+            return -1;
+        }
+    }
     public class Solution
     {
         public int FirstUniqChar(string s)
