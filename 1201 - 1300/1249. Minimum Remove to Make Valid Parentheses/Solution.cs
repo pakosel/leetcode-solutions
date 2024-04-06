b67fef6b05ea9bec8697bd2292bcb30bc6f64102
pakosel <pawel753@gmail.com>
2024-04-06 09:19:04 +0200

    New solution to 1249.

--- 1201 - 1300/1249. Minimum Remove to Make Valid Parentheses/Solution.cs
+++ 1201 - 1300/1249. Minimum Remove to Make Valid Parentheses/Solution.cs
@@ -5,6 +5,38 @@ using System.Text;
 
 namespace MinimumRemoveToMakeValidParentheses
 {
+    public class Solution2024
+    {
+        public string MinRemoveToMakeValid(string s)
+        {
+            int bal = 0;
+            var sb = new StringBuilder();
+            foreach (var c in s)
+            {
+                sb.Append(c);
+                if (c == '(')
+                    bal++;
+                else if (c == ')')
+                {
+                    if (bal > 0)
+                        bal--;
+                    else
+                        sb.Length--;
+                }
+            }
+            int pos = sb.Length - 1;
+            while (bal > 0)
+            {
+                while (sb[pos] != '(')
+                    pos--;
+                sb.Remove(pos, 1);
+                bal--;
+                pos--;
+            }
+            return sb.ToString();
+        }
+    }
+    
     public class Solution
     {
         public string MinRemoveToMakeValid(string s)
--- 1201 - 1300/1249. Minimum Remove to Make Valid Parentheses/SolutionTest.cs
+++ 1201 - 1300/1249. Minimum Remove to Make Valid Parentheses/SolutionTest.cs
@@ -11,23 +11,33 @@ namespace MinimumRemoveToMakeValidParentheses
     {
         private static readonly object[] testCases =
         {
-            new object[] { "a", "a" },
-            new object[] { "(", "" },
-            new object[] { ")", "" },
-            new object[] { "lee(t(c)o)de)", "lee(t(c)o)de" },
-            new object[] { "a)b(c)d", "ab(c)d" },
-            new object[] { "))((", "" },
-            new object[] { "(a(b(c)d)", "a(b(c)d)" },
+            new object[] { "a", new string[] {"a"} },
+            new object[] { "(", new string[] {""} },
+            new object[] { ")", new string[] {""} },
+            new object[] { "lee(t(c)o)de)", new string[] {"lee(t(c)o)de"} },
+            new object[] { "a)b(c)d", new string[] {"ab(c)d"} },
+            new object[] { "))((", new string[] {""} },
+            new object[] { "(a(b(c)d)", new string[] {"a(b(c)d)", "(a(bc)d)"} },
         };
 
         [Test]
         [TestCaseSource("testCases")]
-        public void Test_Generic(string s, string expected)
+        public void Test_Generic2024(string s, string[] expected)
+        {
+            var sol = new Solution2024();
+            var res = sol.MinRemoveToMakeValid(s);
+
+            Assert.That(res, Is.AnyOf(expected));
+        }
+
+        [Test]
+        [TestCaseSource("testCases")]
+        public void Test_Generic(string s, string[] expected)
         {
             var sol = new Solution();
             var res = sol.MinRemoveToMakeValid(s);
 
-            Assert.That(expected == res);
+            Assert.That(res, Is.AnyOf(expected));
         }
     }
 }
